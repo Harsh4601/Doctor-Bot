3b19523a5f4eb141613e72306e0106e636264e3b
“gokulnair2001” <gokulnair.2001@gmail.com>
2021-01-24 01:35:35 +0530

    first view added

---
+++ Doctor Bot/View/DiagnoseViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DiagnoseViewController.swift
+//  Doctor Bot
+//
+//  Created by Gokul Nair on 24/01/21.
+//
+
+import UIKit
+
+class DiagnoseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
